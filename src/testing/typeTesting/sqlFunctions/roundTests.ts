b810dc44953f781a1e7721c11db1ef2d75dbec13
Ali Emre Taşkın <aliemre1990@hotmail.com>
2026-02-19 22:33:10 +0300

    Added round function tests.

--- src/testing/typeTesting/sqlFunctions/roundTests.ts
+++ src/testing/typeTesting/sqlFunctions/roundTests.ts
@@ -1,23 +1,43 @@
-import type { IComparable } from "../../../query/_interfaces/IComparable.js";
 import type ColumnSQLFunction from "../../../query/functions/_functions.js";
-import { employeesTable } from "../../_tables.js";
+import type QueryParam from "../../../query/param.js";
+import { empSalaryQC } from "../../_columns.js";
+import { paramTester, roundTester } from "../../_functions.js";
+import type { AssertEqual, AssertTrue } from "../_typeTestingUtilities.js";
 
-const res = employeesTable.select((cols, { round }) => {
+const roundValues = roundTester(1.55, 1);
+type typeof_RoundValues = typeof roundValues;
+type typeof_RoundValues_ReturnType = typeof_RoundValues extends ColumnSQLFunction<any, any, any, infer TReturnType, any, any, any, any> ? TReturnType : never;
+type roundValues_ReturnType_Test = AssertTrue<AssertEqual<typeof_RoundValues_ReturnType, number>>;
 
-    const res = round(cols.employees.salary, 2).as("calculatedColumn");
+const roundNonNullParams = roundTester(paramTester("val").type<number>(), paramTester("decimalPlaces").type<number>());
+type typeof_RoundNonNullParams = typeof roundNonNullParams;
+type typeof_RoundNonNullParams_ReturnType = typeof_RoundNonNullParams extends ColumnSQLFunction<any, any, any, infer TReturnType, any, any, any, any> ? TReturnType : never;
+type typeof_RoundNonNullParams_Params = typeof_RoundNonNullParams extends ColumnSQLFunction<any, any, any, any, infer TParams, any, any, any> ? TParams : never;
+type typeof_RoundNonNullParams_ValueParamName = typeof_RoundNonNullParams_Params[0] extends QueryParam<any, infer TName, any, any, any, any> ? TName : never;
+type typeof_RoundNonNullParams_ValueParamType = typeof_RoundNonNullParams_Params[0] extends QueryParam<any, any, infer TValueType, any, any, any> ? TValueType : never;
+type typeof_RoundNonNullParams_DecimalPlaceParamName = typeof_RoundNonNullParams_Params[1] extends QueryParam<any, infer TName, any, any, any, any> ? TName : never;
+type typeof_RoundNonNullParams_DecimalPlaceParamType = typeof_RoundNonNullParams_Params[1] extends QueryParam<any, any, infer TValueType, any, any, any> ? TValueType : never;
+type roundNonNullParams_ReturnType_Test = AssertTrue<AssertEqual<typeof_RoundNonNullParams_ReturnType, number>>;
+type roundNonNullParams_ParamsLength_Test = AssertTrue<AssertEqual<typeof_RoundNonNullParams_Params["length"], 2>>;
+type roundNonNullParams_ValueParamName_Test = AssertTrue<AssertEqual<typeof_RoundNonNullParams_ValueParamName, 'val'>>;
+type roundNonNullParams_ValueParamType_Test = AssertTrue<AssertEqual<typeof_RoundNonNullParams_ValueParamType, number>>;
+type roundNonNullParams_DecimalPlaceParamName_Test = AssertTrue<AssertEqual<typeof_RoundNonNullParams_DecimalPlaceParamName, 'decimalPlaces'>>;
+type roundNonNullParams_DecimalPlaceParamType_Test = AssertTrue<AssertEqual<typeof_RoundNonNullParams_DecimalPlaceParamType, number>>;
 
-    return [round(cols.employees.salary, 2)];
-}
-).exec;
+const roundParamsNotTyped = roundTester(paramTester("value"), paramTester("decimalPlaces"));
+type typeof_RoundParamsNotTyped = typeof roundParamsNotTyped;
+type typeof_RoundParamsNotTyped_ReturnType = typeof_RoundParamsNotTyped extends ColumnSQLFunction<any, any, any, infer TReturnType, any, any, any, any> ? TReturnType : never;
+type typeof_RoundParamsNotTyped_Params = typeof_RoundParamsNotTyped extends ColumnSQLFunction<any, any, any, any, infer TParams, any, any, any> ? TParams : never;
+type typeof_RoundParamsNotTyped_ValueParamType = typeof_RoundParamsNotTyped_Params[0] extends QueryParam<any, any, infer TValueType, any, any, any> ? TValueType : never;
+type roundParamsNotTyped_ReturnType_Test = AssertTrue<AssertEqual<typeof_RoundParamsNotTyped_ReturnType, number | null>>;
+type roundParamsNotTyped_ValueParamType_Test = AssertTrue<AssertEqual<typeof_RoundParamsNotTyped_ValueParamType, number | null>>;
 
+const roundParamsValNull = roundTester(paramTester("value"), 2);
+type typeof_RoundParamsValNull = typeof roundParamsValNull;
+type typeof_RoundParamsValNull_ReturnType = typeof_RoundParamsValNull extends ColumnSQLFunction<any, any, any, infer TReturnType, any, any, any, any> ? TReturnType : never;
+type roundParamsValNull_ReturnType_Test = AssertTrue<AssertEqual<typeof_RoundParamsValNull_ReturnType, number | null>>;
 
-const res1 = employeesTable.select((cols, { round, coalesce, param }) => {
-    const calculatedColumn = round(param("asd").type<number>(), coalesce(cols.employees.salary, null)).as("calculatedColumn");
-
-    type t = typeof calculatedColumn;
-    type args = t extends ColumnSQLFunction<any, any, infer args, any, any, any, any, any> ? args : never;
-    type param = args[1];
-    type finalType = param extends IComparable<any, any, any, infer TFinalType, any, any, any> ? TFinalType : never;
-
-    return [calculatedColumn];
-}).exec;
+const roundParamsValNullableCol = roundTester(empSalaryQC, 2);
+type typeof_RoundParamsValNullableCol = typeof roundParamsValNullableCol;
+type typeof_RoundParamsValNullableCol_ReturnType = typeof_RoundParamsValNullableCol extends ColumnSQLFunction<any, any, any, infer TReturnType, any, any, any, any> ? TReturnType : never;
+type roundParamsValNullableCol_ReturnType_Test = AssertTrue<AssertEqual<typeof_RoundParamsValNullableCol_ReturnType, number | null>>;
