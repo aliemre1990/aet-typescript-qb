e122bfd7db44cc064b10a03ab005b7f6c14aafdb
Ali Emre Taşkın <aliemre1990@hotmail.com>
2026-03-08 23:20:27 +0300

    Added other type tests to testing file.

--- src/testing/queryBuilding/000_typeTests.test.ts
+++ src/testing/queryBuilding/000_typeTests.test.ts
@@ -1,12 +1,21 @@
 // comparisons
-import '../typeTesting/comparisons/eq.js'
-import '../typeTesting/comparisons/between.js'
-import '../typeTesting/comparisons/in.js'
+import '../typeTesting/comparisons/eq.js';
+import '../typeTesting/comparisons/between.js';
+import '../typeTesting/comparisons/in.js';
 
 // functions
-import '../typeTesting/sqlFunctions/coalesceTests.js'
+import '../typeTesting/sqlFunctions/coalesceTests.js';
+import '../typeTesting/sqlFunctions/jsonBuildObjectTests.js';
+import '../typeTesting/sqlFunctions/literalValueTests.js';
+import '../typeTesting/sqlFunctions/roundTests.js';
 
 // common
-import '../typeTesting/caseTests.js'
-import '../typeTesting/castTests.js'
-import '../typeTesting/cteTests.js'
+import '../typeTesting/caseTests.js';
+import '../typeTesting/castTests.js';
+import '../typeTesting/combineTests.js'
+import '../typeTesting/cteTests.js';
+import '../typeTesting/groupByTests.js';
+import '../typeTesting/logicalOperationsTests.js';
+import '../typeTesting/orderByTests.js';
+import '../typeTesting/selectTests.js';
+import '../typeTesting/subQueryTests.js';
