5e12ea4fb02afff061a14200d7305bc0136b4345
Ali Emre Taşkın <aliemre1990@hotmail.com>
2026-03-08 23:27:40 +0300

    Refactor subquery tests.

--- src/testing/typeTesting/subQueryTests.ts
+++ src/testing/typeTesting/subQueryTests.ts
@@ -3,46 +3,25 @@ import { customersTable, employeesTable, ordersTable } from "../_tables.js";
 import type { AssertEqual, AssertTrue } from "../_typeTestingUtilities.js";
 
 const subQuery = customersTable
-    .where((cols, { param }) => cols.customers.id.eq(param("whereparam")))
+    .where((cols, { param }) => cols.customers.id.eq(param("whereParam")))
     .select((cols, { round, param }) => ([cols.customers.id, round(cols.customers.createdBy, param("roundParam1")).as("roundResult")]))
     .as("sq1");
-
-/**
- * 
- */
-const MultipleFromQueryWithSubQuery = from(employeesTable.as("employeesAs"), subQuery)
+const multipleFromQuery_WithSubQuery = from(employeesTable.as("employeesAs"), subQuery)
     .select((tab) => [tab.sq1.id])
     .exec
+type typeof_MultipleFromQuery_WithSubQuery_ResultType = ReturnType<typeof multipleFromQuery_WithSubQuery>
+type typeof_MultipleFromQuery_WithSubQuery_ParamsResult = typeof multipleFromQuery_WithSubQuery extends (param: infer TParams) => any ? TParams : never;
+type typeof_MultipleFromQuery_WithSubQuery_ResultType_Expected = { id: number }[]
+type typeof_MultipleFromQuery_WithSubQuery_ParamsResult_Expected = { [key: string]: any; } & { whereParam: number | null, roundParam1: number | null }
+type multipleFromQuery_WithSubQuery_ResultType_Test = AssertTrue<AssertEqual<typeof_MultipleFromQuery_WithSubQuery_ResultType, typeof_MultipleFromQuery_WithSubQuery_ResultType_Expected>>;
+type multipleFromQuery_WithSubQuery_Params_Test = AssertTrue<AssertEqual<typeof_MultipleFromQuery_WithSubQuery_ParamsResult, typeof_MultipleFromQuery_WithSubQuery_ParamsResult_Expected>>
 
-type MultipleFromQueryWithSubQueryResult = {
-    id: number
-}[]
-type MultipleFromQueryWithSubQueryReturnType = ReturnType<typeof MultipleFromQueryWithSubQuery>
-type MultipleFromQueryWithSubQueryTest = AssertTrue<AssertEqual<MultipleFromQueryWithSubQueryResult, MultipleFromQueryWithSubQueryReturnType>>;
-
-type MultipleFromQueryWithSubQueryParams = {
-    whereParam: number | null,
-    roundParam1: number | null
-}
-type MultipleFromQueryWithSubQueryParamsResult = typeof MultipleFromQueryWithSubQuery extends (param: infer TParams) => any ? TParams : never;
-type MultipleFromQueryWithSubQueryParamsTest = AssertTrue<AssertEqual<MultipleFromQueryWithSubQueryParams, MultipleFromQueryWithSubQueryParams>>
-
-
-
-/**
- * 
- */
-const SubQueryJoin = customersTable
+const subQueryJoin = customersTable
     .join('INNER', () => employeesTable, cols => cols.employees.id.eq(cols.customers.id))
     .join('LEFT', () => subQuery, cols => cols.sq1.id.eq(cols.customers.id))
-    .select(cols => [cols.sq1.id])
-    .exec;
+    .select(cols => [cols.sq1.id])    ;
 
-
-/**
- * 
- */
-const SubQuerySelect = customersTable
+const selectSubQuery = customersTable
     .select((tables) => [
         tables.customers.id,
         ordersTable
@@ -52,4 +31,3 @@ const SubQuerySelect = customersTable
             .where((tabInner) => tables.customers.id.eq(tabInner.orders.customerId))
             .as("sqSelect")
     ])
-    .exec
